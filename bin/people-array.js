bd5473c18b2f26b6eaea6e0e445b560751c6b798
jordanallain <jallain@gmail.com>
2017-04-09 22:44:26 -0400

    Run command line tasks for `js` and `md`
    - `grunt make-standard`
    - `grunt nag`
    - `remark` for mark-down files

--- bin/people-array.js
+++ bin/people-array.js
@@ -1,8 +1,8 @@
-'use strict';
+'use strict'
 
-let loadPeople = require('../lib/people.js');
+const loadPeople = require('../lib/people.js')
 
 loadPeople().then((people) => {
-  let count = people.reduce((m /*, p */) => m + 1, 0);
-  console.log(`count: ${count}, length: ${people.length}`);
-}).catch((error) => console.log(error));
+  const count = people.reduce((m /*, p */) => m + 1, 0)
+  console.log(`count: ${count}, length: ${people.length}`)
+}).catch((error) => console.log(error))
--- spec/fibonacci.spec.js
+++ spec/fibonacci.spec.js
@@ -1,37 +1,37 @@
-'use strict';
+'use strict'
 
 // Allow chai syntax like `expect(foo).to.be.ok;`
 // jshint -W030
 
-const chai = require('chai');
-const chaiAsPromised = require('chai-as-promised');
+const chai = require('chai')
+const chaiAsPromised = require('chai-as-promised')
 
-chai.use(chaiAsPromised);
+chai.use(chaiAsPromised)
 
-const expect = chai.expect;
+const expect = chai.expect
 
-const fibResults = require('../lib/fibonacci.js');
+const fibResults = require('../lib/fibonacci.js')
 
 describe('subFibs', function () {
   it('is the sum of the fibs array', function () {
-    expect(fibResults.sumFibs).to.equal(88);
-  });
-});
+    expect(fibResults.sumFibs).to.equal(88)
+  })
+})
 
 describe('prodFibs', function () {
-  it('is the product of the fibs array excluding 0', function (){
-    expect(fibResults.prodFibs).to.equal(2227680);
-  });
-});
+  it('is the product of the fibs array excluding 0', function () {
+    expect(fibResults.prodFibs).to.equal(2227680)
+  })
+})
 
 describe('sumOdd', function () {
   it('is the sum of the odd numbers in the fibs array', function () {
-    expect(fibResults.sumOdd).to.equal(44);
-  });
-});
+    expect(fibResults.sumOdd).to.equal(44)
+  })
+})
 
 describe('prodEven', function () {
-  it('is the product of the even numbers in the fibs array excluding 0', function (){
-    expect(fibResults.prodEven).to.equal(544);
-  });
-});
+  it('is the product of the even numbers in the fibs array excluding 0', function () {
+    expect(fibResults.prodEven).to.equal(544)
+  })
+})
